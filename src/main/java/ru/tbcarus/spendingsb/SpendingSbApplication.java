a276ae8f8e199264c0f91106a1fca528c61920d1
tbcarus <70680359+tbcarus@users.noreply.github.com>
2023-07-13 21:50:51 +0300

    Initial commit

---
+++ src/main/java/ru/tbcarus/spendingsb/SpendingSbApplication.java
@@ -0,0 +1,13 @@
+package ru.tbcarus.spendingsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpendingSbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpendingSbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/tbcarus/spendingsb/SpendingSbApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tbcarus.spendingsb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpendingSbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
