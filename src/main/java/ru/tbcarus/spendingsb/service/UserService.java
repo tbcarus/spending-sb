6679ca8f2caf387975ebf3542d2e0e966581a6d5
tbcarus <70680359+tbcarus@users.noreply.github.com>
2023-07-30 16:18:26 +0300

    User service

---
+++ src/main/java/ru/tbcarus/spendingsb/service/UserService.java
@@ -0,0 +1,43 @@
+package ru.tbcarus.spendingsb.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import ru.tbcarus.spendingsb.model.User;
+import ru.tbcarus.spendingsb.repository.UserRepository;
+
+import java.util.List;
+
+@Service
+@Slf4j
+public class UserService {
+    public final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User create(User user) {
+        return userRepository.save(user);
+    }
+
+    public List<User> getAll() {
+        return userRepository.findAll();
+    }
+
+    public User getById(int id) {
+        return userRepository.getReferenceById(id);
+    }
+
+    public User getByEmail(String email) {
+        return userRepository.getByEmail(email);
+    }
+
+    public User update(User user) {
+        return userRepository.save(user);
+    }
+
+    public void delete(int id) {
+        userRepository.deleteById(id);
+    }
+
+}
