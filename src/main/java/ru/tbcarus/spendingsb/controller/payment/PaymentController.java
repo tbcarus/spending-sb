0c6897398b65fbfcc469814b899d370731af9922
tbcarus <70680359+tbcarus@users.noreply.github.com>
2023-09-01 06:49:03 +0300

    list view init

--- src/main/java/ru/tbcarus/spendingsb/controller/payment/PaymentController.java
+++ src/main/java/ru/tbcarus/spendingsb/controller/payment/PaymentController.java
@@ -3,6 +3,7 @@ package ru.tbcarus.spendingsb.controller.payment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.tbcarus.spendingsb.model.Payment;
 import ru.tbcarus.spendingsb.model.PaymentType;
@@ -25,5 +26,11 @@ public class PaymentController extends AbstractPaymentController {
         model.addAttribute("sumMapByType", sumMapByType);
         model.addAttribute("sumAll", PaymentsUtil.getSumAll(sumMapByType));
         return "list";
+    }
+
+    @GetMapping("/{id}")
+    public String get(Model model, @PathVariable int id) {
+
+        return "edit";
     }
 }
