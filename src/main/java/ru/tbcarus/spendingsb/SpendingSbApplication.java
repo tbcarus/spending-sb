98c30105a3373a5447be73f6e8bd9e98ab5cb06d
tbcarus <70680359+tbcarus@users.noreply.github.com>
2024-06-17 21:19:58 +0300

    To war

--- src/main/java/ru/tbcarus/spendingsb/SpendingSbApplication.java
+++ src/main/java/ru/tbcarus/spendingsb/SpendingSbApplication.java
@@ -2,12 +2,17 @@ package ru.tbcarus.spendingsb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpendingSbApplication {
+public class SpendingSbApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SpendingSbApplication.class, args);
     }
 
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpendingSbApplication.class);
+    }
 }
